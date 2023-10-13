0869c2957444fdb4867dd393e144a556486e3ab6
Ayaka Nagata <t21cs035@yamanashi.ac.jp>
2023-10-13 14:59:35 +0900

    Gitの復習のために、hello.pyをコミット

---
+++ hello.py
@@ -0,0 +1,19 @@
+'''
+Created on 2023/10/13
+
+@author: t21cs035
+'''
+print("Hello, Python World")
+
+import math
+print(math.pi)
+
+
+from fibonacchi import fibo
+print(fibo(20))
+
+from quick_sort import quick_sort
+data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+print(quick_sort(data))
+      
+print(isinstance(2, int))
